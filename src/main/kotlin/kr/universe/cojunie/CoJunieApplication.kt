3c15aa76e5b4a00884216a0940c7f82f5898ddd9
acacian-24 <gkehdrn95@naver.com>
2025-06-13 17:01:31 +0900

    Initial commit

---
+++ src/main/kotlin/kr/universe/cojunie/CoJunieApplication.kt
@@ -0,0 +1,11 @@
+package kr.universe.cojunie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoJunieApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoJunieApplication>(*args)
+}
